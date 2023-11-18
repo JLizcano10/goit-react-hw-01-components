dd489c34a923f93ecb0e0721b684066c775d3640
JL10 <126992358+JLizcano10@users.noreply.github.com>
2023-11-18 02:05:07 -0500

    cambio en el gap del div en App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -19,7 +19,7 @@ export const App = () => {
         fontSize: 40,
         color: '#010101',
         flexDirection: 'column',
-        gap: '30px',
+        gap: '60px',
       }}
     >
       <Profile
